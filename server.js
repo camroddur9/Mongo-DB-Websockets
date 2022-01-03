246caa7f8d288bf6c4938d215858cbf93388f283
Camilo <crodriguez@umbra-team.com>
2022-01-02 20:38:45 -0500

    updated code

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const router = express.Router()
 
 var app = express()
 
-app.use('/', (req, res) => res.send('Hallo'))
+app.use(router);
+
+router.get('/', (req, res) => {res.send('hallo get')})
+router.post('/', (req, res) => {res.send('hallo post')})
+router.delete('/', (req, res) => {res.send('hallo delete')})
+router.patch('/', (req, res) => {res.send('hallo patch')})
 
 app.listen('3000')
 console.log('app listening on http://localhost:3000')
