f620b57562debfbdda5b14acdf676cc52cc89241
Camilo <crodriguez@umbra-team.com>
2022-01-02 20:48:17 -0500

    adedd some querys and body text

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 var app = express()
-
+app.use(bodyParser.json())
 app.use(router);
 
-router.get('/', (req, res) => {res.send('hallo get')})
-router.post('/', (req, res) => {res.send('hallo post')})
-router.delete('/', (req, res) => {res.send('hallo delete')})
-router.patch('/', (req, res) => {res.send('hallo patch')})
+router.get('/message', (req, res) => {
+    res.send('Get message')
+})
+router.post('/message', (req, res) => {
+    console.log(req.query)
+    console.log(req.body)
+    res.send('Added ' + req.body.text + ' message')
+})
 
 app.listen('3000')
 console.log('app listening on http://localhost:3000')
