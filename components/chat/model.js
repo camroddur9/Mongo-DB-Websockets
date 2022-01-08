e47f2cd94d1e3aa541d1be41784298a8ccafda0f
Camilo <crodriguez@umbra-team.com>
2022-01-08 17:44:40 -0500

    fixed security issue

---
+++ components/chat/model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const mySchema = new Schema({
+
+})
+
+const model = moongose.model('Chat', mySchema)
+
+module.exports = model
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./db')
 
 const router = require('./network/routes')
 
-db('mongodb://crodrig53090:Loquendo890%40@mongodbwebsockets-shard-00-00.3hk85.mongodb.net:27017,mongodbwebsockets-shard-00-01.3hk85.mongodb.net:27017,mongodbwebsockets-shard-00-02.3hk85.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-czio4j-shard-0&authSource=admin&retryWrites=true&w=majority')
+db('mongodb://crodrig53090:pass@mongodbwebsockets-shard-00-00.3hk85.mongodb.net:27017,mongodbwebsockets-shard-00-01.3hk85.mongodb.net:27017,mongodbwebsockets-shard-00-02.3hk85.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-czio4j-shard-0&authSource=admin&retryWrites=true&w=majority')
 
 var app = express()
 app.use(bodyParser.json())
